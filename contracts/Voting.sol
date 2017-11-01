55f4a7baa097a8ed5d3fe65d1b8d694ebf2788c5
Robert Askam <robertaskam@lynne-pc.glasscare.local>
2017-11-01 09:30:10 +0000

    Changed Contract to Represent ICO intead of users

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,30 +2,30 @@ pragma solidity ^0.4.11;
 // We have to specify what version of compiler this code will compile with
 
 contract Voting {
-    struct User {
+    struct ICO {
         string name;
         uint votes;
     }
-    User[] public users;
+    ICO[] public icos;
 
     function addIco(string _name) public returns(uint) {
-        users.length++;
-        users[users.length-1].name = _name;
-        users[users.length-1].votes = 0;
-        return users.length;
+        icos.length++;
+        icos[icos.length-1].name = _name;
+        icos[icos.length-1].votes = 0;
+        return icos.length;
     }
 
     function getIcoCount() public constant returns(uint) {
-        return users.length;
+        return icos.length;
     }
 
     function getIco(uint index) public constant returns(string, uint, uint) {
-        return ( users[index].name, users[index].votes, index);
+        return ( icos[index].name, icos[index].votes, index);
     }
 
     function voteForIco(uint icoId) public returns(uint) {
-      users[icoId].votes++;
-      return users[icoId].votes;
+      icos[icoId].votes++;
+      return icos[icoId].votes;
     }
 
 }
